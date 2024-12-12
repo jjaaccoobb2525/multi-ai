f43c2fdbf3b340e6968e287256a5966357746935
Choi Chan Hwan <africakokiri@gmail.com>
2024-12-12 15:14:59 +0900

    작동 테스트용 컴포넌트 추가
    - 작동을 테스트하기 위한 컴포넌트를 추가했습니다.

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,3 +1,17 @@
+import { Textarea } from "@/modules/components/textarea";
+
 export default function MainPage() {
-  return <main></main>;
+  return (
+    <main className="flex h-full w-full flex-col justify-center p-8">
+      <div className="bg-red-200 flex h-full justify-between">
+        <div>1</div>
+        <div>2</div>
+        <div>3</div>
+        <div>4</div>
+      </div>
+      <div className="flex w-full justify-center">
+        <Textarea className="w-1/2 resize-none" />
+      </div>
+    </main>
+  );
 }
--- frontend/tailwind.config.ts
+++ frontend/tailwind.config.ts
@@ -5,17 +5,7 @@ const config: Config = {
   darkMode: ["class"],
   content: ["./src/**/*.{js,ts,jsx,tsx,mdx}"],
   theme: {
-    extend: {
-      colors: {
-        // neutral 900
-        black: "hsl(0, 0%, 9%)",
-        // neutral 100
-        white: "hsl(0, 0%, 96.1%)",
-        red: "hsl(0, 84.2%, 60.2%)",
-        yellow: "hsl(45.39999999999998, 93.4%, 47.5%)",
-        green: "hsl(142.10000000000002, 70.6%, 45.3%)"
-      }
-    }
+    extend: {}
   },
   plugins: [twAnimate]
 };
