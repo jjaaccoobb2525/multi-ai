f19a5308fdfcdf0f39720a6c37f6493c23771e55
Choi Chan Hwan <africakokiri@gmail.com>
2024-12-14 13:38:06 +0900

    position 테스트
    - main 태그의 position absolute 테스트입니다.

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -4,7 +4,8 @@ import UserInputArea from "@/modules/user-input";
 export default function MainPage() {
   return (
     <main className="absolute flex h-full w-[200vw] flex-col items-start gap-8 p-8">
-      <div className="flex h-full w-full justify-between gap-8">
+      <div className="">test</div>
+      <div className="flex h-[90%] w-full justify-between gap-8">
         {/* 사용자의 Input 영역 */}
         <UserInputArea />
         {/* 사용자의 Input 영역 */}
